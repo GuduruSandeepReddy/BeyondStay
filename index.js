fa94a1e5b64bd8f996e39d4c2b1c5254b36e7d92
GuduruSandeepReddy <sandeepreddygvsk@gmail.com>
2024-08-18 03:55:29 -0700

    Error handling updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
 
@@ -6,77 +6,67 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-const session = require("express-session")
+const session = require("express-session");
 const MongoStore = require("connect-mongo");
- const path = require("path");
- const ejsmate = require("ejs-mate");
- const flash = require("connect-flash");
- const multer = require("multer")
- 
-const  {listingSchema, reviewSchema} = require("./schema.js");
- const methodOverride = require("method-override");
+const path = require("path");
+const ejsmate = require("ejs-mate");
+const flash = require("connect-flash");
+const multer = require("multer");
+
+const { listingSchema, reviewSchema } = require("./schema.js");
+const methodOverride = require("method-override");
 const wrapAsync = require("./utils/wrapAsync.js");
- const ExpressError = require("./utils/ExpressError.js");
- const Review = require("./models/review.js");
+const ExpressError = require("./utils/ExpressError.js");
+const Review = require("./models/review.js");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const user = require("./models/user.js");
 
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
-
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
+// const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = "mongodb://127.0.0.1:27017/beyondstay";
 
-main().then(()=>{
-         console.log("connected successfully");
- })
- .catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(dbUrl);
+    console.log("connected successfully");
+}
 
-
- async function main() {
-  await mongoose.connect(dbUrl);
-
-   }
+main().catch(err => console.log(err));
 
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
- app.use(express.urlencoded({extended:true}));
-app.engine('ejs',ejsmate);
- app.use(express.static(path.join(__dirname,"/public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.engine('ejs', ejsmate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(methodOverride("_method"));
 
-
-const store= MongoStore.create({
+const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
-    touchAfter: 24*3600,
+    touchAfter: 24 * 3600,
 });
 
-store.on("error",()=>{
-    console.log("Error in Mongo session store",err);
+store.on("error", (err) => {
+    console.log("Error in Mongo session store", err);
 });
+
 const sessionOptions = {
-   store,
+    store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 7*24*60*60*1000,
-        maxAge: 7*24*60*60*1000,
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true
     }
 };
-
-
-// //home directory
-// app.get("/",(req,res)=>{
-//     res.send("hi , iam sandeep");
-// });
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -88,36 +78,28 @@ passport.use(new LocalStrategy(user.authenticate()));
 passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
-
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
 
-app.use("/listings",listingsRouter);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id", reviewsRouter);
+app.use("/", userRouter);
 
-app.use("/listings/:id",reviewsRouter);
-
-app.use("/",userRouter);
-
-
-app.use((err,req,res,next) => {
-    
-    let {statusCode =500, message= "something went wrong"} = err;
-    
-    res.status(statusCode).render("listings/error.ejs",{err});
-
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
 });
 
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error("Error encountered:", err.message); // Logs the error message
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("listings/error", { err });
+});
 
-app.all("*", (req,res,next) =>{
-
-    next(new ExpressError(404,"Page Not Found"));
-  });
-
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("server is working well");
 });
-
